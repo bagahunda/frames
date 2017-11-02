82f027179a28e3f99a1e6ec7e929f1337aa79d76
Dmitriy <dmitriy.troy@gmail.com>
2017-11-02 16:48:35 +0300

    add basic normalize

--- src/App.vue
+++ src/App.vue
@@ -17,6 +17,10 @@ export default {
   *, *:after, *:before {
     box-sizing: inherit;
   }
+  body {
+    margin: 0;
+    padding: 0;
+  }
   #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
