e54a7abec50bb5b791dda9c2550b2312d016cc32
Dmitriy <dmitriy.troy@gmail.com>
2017-11-02 16:34:59 +0300

    fixes

--- src/components/main.vue
+++ src/components/main.vue
@@ -316,8 +316,8 @@ export default {
   .frames-container label {
     display: block;
     padding: 5px;
-    width: 80px;
-    height: 80px;
+    width: 100%;
+    height: 100%;
     border-radius: 4px;
     border: 1px solid #eee;
   }
